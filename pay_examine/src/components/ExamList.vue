03bdedf30f45971b2dfeed1d56d55dc65608de0d
Irvinglck <lichangkui@chinadaas.com>
2020-08-05 17:07:17 +0800

    分页&搜索

--- pay_examine/src/components/ExamList.vue
+++ pay_examine/src/components/ExamList.vue
@@ -1,25 +1,50 @@
 <template>
   <div class="listData">
-    <el-header style="width: 100%; background-color: #20A0FF">11111111111111</el-header>
+    <el-header style="width: 100%; background-color: #20A0FF"></el-header>
     <el-container>
       <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
         <el-menu>
           <el-menu-item index="2">
             <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
+            <span slot="title">用户实名审核</span>
           </el-menu-item>
-          <el-menu-item index="3" disabled>
+          <el-menu-item index="3">
+            <!--          <el-menu-item index="3" disabled>-->
             <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
+            <span slot="title">保留导航</span>
           </el-menu-item>
           <el-menu-item index="4">
             <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
+            <span slot="title">保留导航</span>
           </el-menu-item>
         </el-menu>
       </el-aside>
       <el-container>
         <el-main class="main-add">
+          <el-form :inline="true" :model="formInline" class="demo-form-inline sl-form">
+            <el-form-item label="姓名">
+              <el-input v-model="formInline.name" placeholder="姓名"></el-input>
+            </el-form-item>
+            <el-form-item label="用户类型">
+              <el-select v-model="formInline.userType" placeholder="用户类型">
+                <el-option label="" value=""></el-option>
+                <el-option label="残障用户" value="1"></el-option>
+                <el-option label="普通用户" value="0"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="审核进度">
+              <el-select v-model="formInline.exam" placeholder="审核进度">
+                <el-option label="" value=""></el-option>
+                <el-option label="通过" value="1"></el-option>
+                <el-option label="未通过" value="0"></el-option>
+                <el-option label="审核中" value="2"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">查询</el-button>
+            </el-form-item>
+          </el-form>
+          <!--数据列表-->
           <el-table
             :data="examAuthList"
             style="width: 100%"
@@ -38,6 +63,16 @@
                 <span class="examine" style="background-color:#84CE60;"
                       v-else-if="scope.row.realAthenNameSign === 1">通过</span>
                 <span class="examine" style="background-color:red;" v-else>未通过</span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              label="是否实名"
+              width="80">
+              <template slot-scope="scope">
+                <span class="examine" style="background-color:red;"
+                      v-if="scope.row.realAthenNameSign === 0">否</span>
+                <span class="examine" style="background-color:#909399;"
+                      v-else-if="scope.row.realAthenNameSign === 1">是</span>
               </template>
             </el-table-column>
             <el-table-column
@@ -86,6 +121,18 @@
 
             </el-table-column>
           </el-table>
+          <!--分页-->
+          <div class="block">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page.sync="currentPage3"
+              :page-size="pageSize"
+              layout="prev, pager, next, jumper"
+              :total="total">
+            </el-pagination>
+          </div>
+          <!--抽屉-->
           <el-drawer
             title="我是标题"
             :visible.sync="drawer"
@@ -125,18 +172,56 @@
         }
       },
       onSubmit() {
-        console.log(this.formInline.user);
+        console.log(this.formInline.user, this.formInline.region);
+        let parms = {name: this.formInline.name, userType: this.formInline.userType, exam: this.formInline.exam}
+        this.$api.get('/sign_technology/examAuthInfo', parms, response => {
+          if (response.status >= 0 && response.status < 300) {
+            console.log(response.data.data);//请求成功，response为成功信息参数
+            this.examAuthList = response.data.data;
+          } else {
+            console.log(response.message);//请求失败，response为失败信息
+          }
+        });
       },
+      //  分页
+      handleSizeChange(val) {
+        console.log(`每页size------ ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页 前后: ${val}`);
+        let params = {
+          name: this.formInline.name,
+          userType: this.formInline.userType,
+          exam: this.formInline.exam,
+          pageNum: val
+        };
+        this.$api.get('/sign_technology/examAuthInfo', params, response => {
+          if (response.status >= 0 && response.status < 300) {
+            console.log(response.data.data);//请求成功，response为成功信息参数
+            this.examAuthList = response.data.data;
+          } else {
+            console.log(response.message);//请求失败，response为失败信息
+          }
+        });
+      }
 
     },
     data() {
       return {
         examAuthList: [],
+        //搜索
         formInline: {
-          user: '',
-          region: ''
+          name: '',
+          userType: '',
+          exam: ''
         },
+        //查看证件照
         drawer: false,
+        //分页
+        currentPage3: 1,//当前页
+        pageSize: 5,//每页条数
+        total:20//总记录数
+
       }
     }
   }
@@ -162,12 +247,13 @@
   }
 
   .main-add {
-    margin-left: 10px;
+    margin-left: 4px;
     padding: 0px;
   }
 
-
-
+  .sl-form {
+    padding-top: 20px;
+  }
 
 
   /*.el-container:nth-child(7) .el-aside {*/
